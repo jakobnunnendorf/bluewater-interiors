464c3ecde3b3dd592dd267c4db3c8d9307603a4c
Jakob <jnunnendorf@icloud.com>
2024-08-03 17:42:20 +0200

    feat(services): add Services.tsx

--- web-app/app/services/page.tsx
+++ web-app/app/services/page.tsx
@@ -1,7 +1,13 @@
-export default function Services() {
+import Hero from "@/app/HomePageComponents/Hero";
+import Services from "./Services"
+import ContactForm from "@/app/contact/ContactForm";
+
+export default function ServicesPage() {
     return (
         <section className="h-fit">
-            services
+            <Hero/>
+            <Services/>
+            <ContactForm/>
         </section>
     );
 }
