8664960b6ff6eb4c9fd86bec77e6c15f87c042eb
Jakob <jnunnendorf@icloud.com>
2024-08-03 22:28:54 +0200

    style(tailwind.config.ts): add gold to presets

--- web-app/tailwind.config.ts
+++ web-app/tailwind.config.ts
@@ -10,11 +10,14 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        'gold': '#FDEBBD', // Add your custom color here
       },
     },
   },
   plugins: [],
 };
+
 export default config;
