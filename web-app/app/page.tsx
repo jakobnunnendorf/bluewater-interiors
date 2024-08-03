e91b1e65a9a7bda4931b8ab0ab0c30cc8d61943d
Jakob <jnunnendorf@icloud.com>
2024-08-03 13:43:33 +0200

    refactor(page.tsx/Portfolio.tsx): separate component for section

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,62 +1,12 @@
 import Image from "next/image";
 import Hero from "./Hero"
+import Portfolio from "./Portfolio"
 
 export default function Home() {
   return (
     <section className="h-fit">
       <Hero/>
-      <div className="bg-slate-400 h-fit relative w-full  text-slate-800">
-        <h2 className="text-6xl text-center font-bold pt-32">Portfolio</h2>
-        <h3 className="text-2xl text-center font-bold pt-8">
-          {" "}
-          State-of-the-art websites for our yacht interior designers
-        </h3>
-        <div className="w-full px-4 h-full flex py-12">
-          <div className="w-1/2  h-fit">
-            <figure className="relative w-full ">
-              <Image
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-                width={900}
-                height={600}
-                src="/pf1.png"
-                alt="test"
-              />
-            </figure>
-            <figure className="relative w-full ">
-              <Image
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-                width={900}
-                height={600}
-                src="/pf3.png"
-                alt="test"
-              />
-            </figure>
-          </div>
-          <div className="w-1/2  h-fit">
-            <figure className="relative w-full">
-              <Image
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-                width={900}
-                height={600}
-                src="/pf2.png"
-                alt="test"
-              />
-            </figure>
-          </div>
-        </div>
-      </div>
+      <Portfolio/>
       <div className="bg-slate-800 text-white">
         <h2 className="text-white  font-extrabold text-4xl text-center p-28 pb-2">
           Guaranteed results
