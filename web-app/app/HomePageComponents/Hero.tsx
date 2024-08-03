8337c4934e7cf44994324e0e7a76fb4d303346b1
Jakob <jnunnendorf@icloud.com>
2024-08-03 17:42:44 +0200

    refactor(video): to yacht-video.mov

--- web-app/app/HomePageComponents/Hero.tsx
+++ web-app/app/HomePageComponents/Hero.tsx
@@ -68,7 +68,7 @@ function Hero() {
                 </ul>
             </div>
             <video autoPlay loop muted className="absolute top-0 w-full z-0 ">
-                <source src="/95m O'PARI Luxury Yacht.mov" type="video/mp4"/>
+                <source src="/yacht-video.mov" type="video/mp4"/>
             </video>
         </div>
     );
