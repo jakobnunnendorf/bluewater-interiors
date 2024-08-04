13315c23b3434ddbea195dedc637fc2066189746
Jakob <jnunnendorf@icloud.com>
2024-08-05 00:55:14 +0200

    style(ValueProposition.tsx): make nice

--- web-app/app/HomePageComponents/ValueProposition/Pain.tsx
+++ web-app/app/HomePageComponents/ValueProposition/Pain.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function Pain() {
-    return (
-        <div className="border rounded-lg bg-slate-400 p-8">
-            <h3 className="text-2xl font-bold text-center pb-6">Have you ever felt like a&nbsp;
-                <span className="text-[#FDEBBD]">project</span> passed you by?
-            </h3>
-            <ul className="flex flex-col gap-8">
-                <li>Does your portfolio get the <span className="font-bold text-[#FDEBBD]">spotlight</span> it
-                    deserves?
-                </li>
-                <li>Does your website do <span className="font-bold text-[#FDEBBD]">your portfolio</span> justice?
-                </li>
-                <li>Did your old web agency truly understand your <span className="font-bold text-[#FDEBBD]">design philosophy?</span>
-                </li>
-                <li>Are your customers <span className="font-bold text-[#FDEBBD]">STUNNED</span> from the&nbsp;
-                    first click
-                    to your first portfolio project?
-                </li>
-                <li>You want a better website but nobody has&nbsp;
-                    <span className="font-bold text-[#FDEBBD]">experience</span> working with a&nbsp;
-                    <span className="font-bold text-[#FDEBBD]">yacht interior designer</span>?
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="border p-8 md:rounded-lg md:bg-gray-200">
+      <h3 className="pb-6 text-center text-2xl font-bold">
+        Ever felt like projects are passing you by?
+      </h3>
+      <ul className="flex flex-col gap-8">
+        <li>
+          <span className="text-xl">👀</span> Does your portfolio get the&nbsp;
+          <span className="font-bold">spotlight</span> it deserves?
+        </li>
+        <li>
+          <span className="text-xl">🚫</span> Does your website do{" "}
+          <span className="font-bold">your portfolio</span> justice?
+        </li>
+        <li>
+          <span className="text-xl">🧠</span> Did your old web agency truly
+          understand your <span className="font-bold">design philosophy?</span>
+        </li>
+        <li>
+          <span className="text-xl">😓</span> You want a better website but
+          nobody has&nbsp;
+          <span className="font-bold">experience</span> working with a&nbsp;
+          <span className="font-bold">yacht interior designer</span>?
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Pain;
