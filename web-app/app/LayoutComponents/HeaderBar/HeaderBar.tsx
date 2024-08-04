d535d054025add542cfc116015daa13b5cc13aa7
Jakob <jnunnendorf@icloud.com>
2024-08-04 12:29:18 +0200

    style(HeaderBar): make responsive

--- web-app/app/LayoutComponents/HeaderBar/HeaderBar.tsx
+++ web-app/app/LayoutComponents/HeaderBar/HeaderBar.tsx
@@ -4,7 +4,7 @@ import Navigation from "@/app/LayoutComponents/HeaderBar/Navigation";
 
 function HeaderBar() {
   return (
-    <header className="fixed top-0 z-50 grid h-28 w-full grid-cols-3 items-center justify-between bg-white md:flex md:h-40 md:bg-none md:p-4 md:px-8">
+    <header className="fixed top-0 z-50 grid h-48 w-full grid-cols-3 place-content-center place-items-center bg-white md:flex md:h-40 md:items-center md:justify-between md:bg-transparent md:p-4 md:px-8">
       <Logo />
       <Navigation />
     </header>
--- web-app/app/LayoutComponents/HeaderBar/Navigation.tsx
+++ web-app/app/LayoutComponents/HeaderBar/Navigation.tsx
@@ -8,8 +8,8 @@ function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen(!isOpen);
   const style = isOpen
-    ? "flex flex-col -translate-x-[100vw]"
-    : "translate-none";
+    ? "translate-none"
+    : "flex flex-col -translate-x-[100vw]";
   const links = [
     { name: "Home", link: "/" },
     { name: "About", link: "/about" },
@@ -18,13 +18,13 @@ function Navigation() {
     { name: "Contact", link: "/contact" },
   ];
   return (
-    <nav className="col-start-1 row-start-1 h-fit w-fit md:w-1/2">
+    <nav className="col-start-1 row-start-1 mx-8 my-auto h-fit w-fit place-self-start md:w-1/2">
       <Hamburger isOpen={isOpen} toggleOpen={toggleOpen} />
       <ul
-        className={`absolute h-[calc(100vh-112px)] w-[100vw] transition-transform duration-1000 md:static md:h-fit md:w-full ${style} flex flex-col items-center justify-around font-serif text-[#FDEBBD] md:flex-row`}
+        className={`${style} absolute flex h-[calc(100vh-112px)] w-[100vw] flex-col items-center justify-around bg-white font-serif text-[#FDEBBD] transition-transform duration-1000 md:static md:h-fit md:w-full md:translate-x-0 md:flex-row md:bg-transparent`}
       >
         {links.map((link) => (
-          <li key={link.link}>
+          <li className="" key={link.link}>
             <Link href={link.link}>{link.name}</Link>
           </li>
         ))}
