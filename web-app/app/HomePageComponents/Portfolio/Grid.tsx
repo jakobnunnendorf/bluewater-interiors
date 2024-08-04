fcfb326e37886d544105107b91187cbb074b4cc6
Jakob <jnunnendorf@icloud.com>
2024-08-05 00:54:06 +0200

    style(Portfolio.tsx): make nice

--- web-app/app/HomePageComponents/Portfolio/Grid.tsx
+++ web-app/app/HomePageComponents/Portfolio/Grid.tsx
@@ -3,22 +3,69 @@ import Card from "@/app/HomePageComponents/Portfolio/Card";
 
 function Grid() {
   return (
-    <div className="flex h-full w-full px-4 py-12">
-      <div className="h-fit w-1/2">
-        <Card
-          src="/portfolio/pf1.png"
-          alt="Website of the Lürssen Solandge luxury yacht"
+    <div className="grid h-fit w-fit grid-rows-3 gap-8 px-6 py-12 md:grid-cols-2 md:grid-rows-none md:px-40">
+      {articles.map((article, index) => (
+        <Article
+          key={index}
+          src={article.src}
+          alt={article.alt}
+          title={article.title}
+          description={article.description}
         />
-        <Card
-          src="/portfolio/pf3.png"
-          alt="Second version of the Nautical Elegance website"
-        />
-      </div>
-      <div className="h-fit w-1/2">
-        <Card src="/portfolio/pf2.png" alt="Nautical Elegance website" />
-      </div>
+      ))}
     </div>
   );
 }
 
 export default Grid;
+
+const articles = [
+  {
+    src: "/portfolio/pf3.png",
+    alt: "Second version of the Nautical Elegance website",
+    title: "Nautical Elegance",
+    description: `Let us introduce you to Nautical Elegance, a fictional yacht interior
+        design studio that we made up to showcase a few example website
+        designs. It is led by the internationally renown fictional chief
+        designer Sophie Lance.`,
+  },
+  {
+    src: "/portfolio/pf1.png",
+    alt: "Website of the Lürssen Solandge luxury yacht",
+    title: "Lürssen Solandge",
+    description: `With us, all of your clients can get a branded website for their yacht
+      with a booking/availability calendar. And the best part? Completely
+      free of charge. The Lürssen Solandge project was particularly exciting as
+      it allowed us to incorporate luxury design elements that highlight the 
+      elegance and sophistication of the yacht.`,
+  },
+  {
+    src: "/portfolio/pf2.png",
+    alt: "Nautical Elegance website",
+    title: "Nautical Elegance v2",
+    description: `This is the second version of Nautical Elegance, an upgraded and more refined
+      design to better represent the prestigious yacht interior design studio. The project involved
+      a complete overhaul of the user interface to enhance usability and aesthetic appeal.
+      `,
+  },
+];
+
+function Article({
+  src,
+  alt,
+  title,
+  description,
+}: {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+}) {
+  return (
+    <article className="h-full rounded-xl bg-gray-200 p-4 shadow-2xl">
+      <Card src={src} alt={alt} />
+      <h3 className="p-4 pb-2 text-center text-2xl font-bold">{title}</h3>
+      <p className="pb-4">{description}</p>
+    </article>
+  );
+}
