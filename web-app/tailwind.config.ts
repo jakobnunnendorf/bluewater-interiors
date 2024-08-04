b444108e7a1d9de8e4247fa3869e4ee445465f46
Jakob <jnunnendorf@icloud.com>
2024-08-04 15:01:05 +0200

    chore(tailwind): add Archivo Black font for headings

--- web-app/tailwind.config.ts
+++ web-app/tailwind.config.ts
@@ -10,11 +10,13 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'gold': '#FDEBBD', // Add your custom color here
+        gold: "#FDEBBD", // Add your custom color here
       },
+      fontFamily: { fat: ["Archivo Black"] },
     },
   },
   plugins: [],
