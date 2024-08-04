ce620c9f292f93d78719fb7a879d3d49b4f52c62
Jakob <jnunnendorf@icloud.com>
2024-08-05 00:55:00 +0200

    style(BookCall.tsx): add color prop but doesn't work
    For the purpose of this particular use case it worked by accident though

--- web-app/app/LayoutComponents/Hero/BookCall.tsx
+++ web-app/app/LayoutComponents/Hero/BookCall.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import Link from "next/link";
 import InfoTag from "@/app/LayoutComponents/Hero/InfoTag";
 
-function BookCall() {
+function BookCall({ color }: { color?: string }) {
   return (
     <div className="relative flex flex-col py-8 md:block md:w-52">
       <div className="flex h-full items-center justify-center">
         <Link href="https://calendly.com/jakobnunnendorf/call">
-          <div className="mx-auto h-fit w-fit rounded-full border border-blue-400 px-4 py-4 shadow-xl md:my-auto md:mt-0">
-            <h3 className="font-fat leading-3 text-blue-400">Book a call</h3>
+          <div
+            className={`mx-auto h-fit w-fit rounded-full border border-${color ? color : "blue-400"} px-4 py-4 shadow-xl md:my-auto md:mt-0`}
+          >
+            <h3
+              className={`font-fat leading-3 text-${color ? color : "blue-400"}`}
+            >
+              Book a call
+            </h3>
           </div>
         </Link>
       </div>
