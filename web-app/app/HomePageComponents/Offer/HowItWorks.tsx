f95ffe45708d61cd3b590f67941210af806077ab
Jakob <jnunnendorf@icloud.com>
2024-08-05 02:40:10 +0200

    style(Offer.tsx): style responsive

--- web-app/app/HomePageComponents/Offer/HowItWorks.tsx
+++ web-app/app/HomePageComponents/Offer/HowItWorks.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
 function HowItWorks() {
-    return (
-        <div className="w-1/2 mx-auto flex flex-col gap-8">
-            <h2 className="text-4xl font-bold text-center pb-4 pt-16">How can we achieve this?</h2>
-            <h3>Because we serve only one type of customer, we can spend 70% of our time on improving instead of
-                reinventing the wheel. </h3>
-        </div>
-    );
+  return (
+    <div className="mx-auto flex flex-col gap-8 px-6 md:w-1/2 md:px-0">
+      <h2 className="pb-4 text-center text-4xl font-bold">
+        How can we achieve this?
+      </h2>
+      <figure className="mx-auto flex flex-col items-center">
+        <Image src="/avatar.png" alt="Avatar Icon" height={100} width={100} />
+        <figcaption className="text-center text-2xl">
+          One type of customer
+        </figcaption>
+      </figure>
+      <figure className="mx-auto flex flex-col items-center">
+        <Image
+          src="/reinventing-wheel.gif"
+          alt="Avatar Icon"
+          height={140}
+          width={140}
+        />
+        <figcaption className="text-2xl">No reinventing the wheel</figcaption>
+      </figure>
+      <figure className="mx-auto flex flex-col items-center">
+        <Image
+          src="/improvement.jpg"
+          alt="Avatar Icon"
+          height={100}
+          width={100}
+        />
+        <figcaption className="text-2xl">Only improvement</figcaption>
+      </figure>
+    </div>
+  );
 }
 
 export default HowItWorks;
--- web-app/app/HomePageComponents/Offer/ResultsPanel.tsx
+++ web-app/app/HomePageComponents/Offer/ResultsPanel.tsx
@@ -1,73 +1,79 @@
-import React from 'react';
+import React from "react";
 
-interface Result{
-    metric: string,
-    result: string
+interface Result {
+  metric: string;
+  result: string;
 }
 
-function Metric({result}:{result: Result}) {
-    return <div
-        className="text-center grid place-content-center border rounded-xl w-32 h-32 p-8">
-        <h4 className="font-bold pb-2">{result.metric}</h4>
-        <h5 className="text-green-500 ">{result.result}</h5>
+function Metric({ result }: { result: Result }) {
+  return (
+    <div className="grid h-24 w-24 place-content-center rounded-xl border p-8 text-center md:h-32 md:w-32">
+      <h4 className="pb-2 font-bold">{result.metric}</h4>
+      <h5 className="text-green-500">{result.result}</h5>
     </div>
+  );
 }
 
 function ResultsPanel() {
-    const results: Result[] = [
-        {
-            metric: "Revenue",
-            result: "+15%"
-        },
-        {
-            metric: "Profit",
-            result: "+13%"
-        },
-        {
-            metric: "Clicks",
-            result: "+780%"
-        },
-        {
-            metric: "Leads",
-            result: "+70%"
-        },
-        {
-            metric: "Conversion Rate",
-            result: "+25%"
-        },
-        {
-            metric: "Customer Satisfaction",
-            result: "+40%"
-        },
-        {
-            metric: "Market Share",
-            result: "+8%"
-        },
-        {
-            metric: "New Subscribers",
-            result: "+50%"
-        },
-        {
-            metric: "Cost per Acquisition",
-            result: "-20%"
-        },
-        {
-            metric: "Bounce Rate",
-            result: "-15%"
-        }
-    ];
-    return (
-        <div>
-            <h2 className="text-4xl font-bold text-center pb-16">Our average client&apos;s results</h2>
-            <ul className="grid grid-cols-5 gap-6 w-fit mx-auto pb-16">
-                {results.map((result, index) => {
-                    return <li key={index}>
-                        <Metric result={result}/>
-                    </li>
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="pb-10 pt-20">
+      <h2 className="pb-16 text-center font-fat text-4xl font-bold">
+        Our average client&apos;s results
+      </h2>
+      <ul className="mx-auto grid w-fit grid-cols-2 gap-6 pb-16 md:grid-cols-5">
+        {results.map((result, index) => {
+          return (
+            <li key={index}>
+              <Metric result={result} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default ResultsPanel;
+
+const results: Result[] = [
+  {
+    metric: "SEO Ranking",
+    result: "+13%",
+  },
+  {
+    metric: "Traffic",
+    result: "+15%",
+  },
+  {
+    metric: "Speed",
+    result: "+25%",
+  },
+  {
+    metric: "Watch time",
+    result: "+780%",
+  },
+  {
+    metric: "Bounce Rate",
+    result: "-15%",
+  },
+  {
+    metric: "Leads",
+    result: "+70%",
+  },
+  {
+    metric: "Cost per Acquisition",
+    result: "-20%",
+  },
+  {
+    metric: "New Clients",
+    result: "+10%",
+  },
+  {
+    metric: "Revenue",
+    result: "+8%",
+  },
+  {
+    metric: "ROI Time",
+    result: "3 Mo",
+  },
+];
