691d7271169acf81a9a646ef647a48203dded9f0
Jakob <jnunnendorf@icloud.com>
2024-08-05 02:40:01 +0200

    style(Introduction.tsx): style responsive

--- web-app/app/HomePageComponents/Introduction/Introduction.tsx
+++ web-app/app/HomePageComponents/Introduction/Introduction.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import Portrait from "@/app/HomePageComponents/Introduction/Portrait";
 import Bio from "@/app/HomePageComponents/Introduction/Bio";
 
 function Introduction() {
-    return (
-        <div className="grid grid-cols-2 p-32">
-            <Portrait/>
-            <Bio/>
-        </div>
-    );
+  return (
+    <div className="py-24 md:grid md:grid-cols-2 md:p-32">
+      <Portrait />
+      <Bio />
+    </div>
+  );
 }
 
 export default Introduction;
--- web-app/app/HomePageComponents/Introduction/Portrait.tsx
+++ web-app/app/HomePageComponents/Introduction/Portrait.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 
 function Portrait() {
-    return (
-        <figure>
-            <Image src="/portrait.jpg" height={400} width={400} alt="test" className="mx-auto"/>
-            <figcaption className="text-center">Dubai International Boat Show, 2024</figcaption>
-        </figure>
-    );
+  return (
+    <figure className="w-full px-4">
+      <Image
+        src="/portrait.jpg"
+        height={400}
+        width={400}
+        alt="test"
+        className="mx-auto rounded-lg"
+      />
+      <figcaption className="p-4 text-center font-bold">
+        Dubai International Boat Show, 2024
+      </figcaption>
+    </figure>
+  );
 }
 
 export default Portrait;
