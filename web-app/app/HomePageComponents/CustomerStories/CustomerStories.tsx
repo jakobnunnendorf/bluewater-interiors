9608e5b079dee99e8f7c7581c32e39e3f15fdd6d
Jakob <jnunnendorf@icloud.com>
2024-08-05 02:39:47 +0200

    style(CustomerStories.tsx): style responsive & replace w reviews

--- web-app/app/HomePageComponents/CustomerStories/CustomerStories.tsx
+++ web-app/app/HomePageComponents/CustomerStories/CustomerStories.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import Story from "@/app/HomePageComponents/CustomerStories/Story";
 import CustomerReviews from "@/app/HomePageComponents/CustomerReviews/CustomerReviews";
 
 function CustomerStories() {
-  const stories = [
-    { src: "/customer stories/customer 1.webp" },
-    { src: "/customer stories/customer 2.webp" },
-    { src: "/customer stories/customer 3.webp" },
-    { src: "/customer stories/customer 4.webp" },
-  ];
   return (
-    <div className="rborder pt-32">
+    <div className="pt-32">
       <h2 className="text-center text-4xl font-bold">
         Don&apos;t listen to us, <br /> listen to:
       </h2>
