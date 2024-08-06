4abacf4ee7df3b9aa4113c86ee8bf5e78f6fb223
Jakob <jnunnendorf@icloud.com>
2024-08-06 13:15:29 +0200

    style(Portfolio.tsx): enlarge on hover

--- web-app/app/portfolio/Portfolio.tsx
+++ web-app/app/portfolio/Portfolio.tsx
@@ -1,48 +1,51 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const projects = [
-    {
-        title: 'Lürssen Solandge',
-        description: 'Where luxury meets the horizon.',
-        image: '/pf1.png',
-    },
-    {
-        title: 'Nautical Elegance',
-        description: 'Where luxury meets the horizon.',
-        image: '/pf2.png',
-    },
-    {
-        title: 'Nautical Elegance Interior',
-        description: 'Where luxury meets the horizon.',
-        image: '/pf3.png',
-    },
+  {
+    title: "Lürssen Solandge",
+    description: "Where luxury meets the horizon.",
+    image: "/portfolio/pf1.png",
+  },
+  {
+    title: "Nautical Elegance",
+    description: "Where luxury meets the horizon.",
+    image: "/portfolio/pf2.png",
+  },
+  {
+    title: "Nautical Elegance Interior",
+    description: "Where luxury meets the horizon.",
+    image: "/portfolio/pf3.png",
+  },
 ];
 
 const Portfolio: React.FC = () => {
-    return (
-        <div className="min-h-screen py-8">
-            <h1 className="text-4xl font-bold text-center mb-8">Our Projects</h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 container mx-auto px-4">
-                {projects.map((project, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
-                        <div className="relative w-full h-64">
-                            <Image
-                                src={project.image}
-                                alt={project.title}
-                                layout="fill"
-                                objectFit="cover"
-                                className="rounded-t-lg"
-                            />
-                        </div>
-                        <div className="p-6">
-                            <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
-                            <p className="text-gray-700">{project.description}</p>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="min-h-screen py-8">
+      <h1 className="mb-8 text-center text-4xl font-bold">Our Projects</h1>
+      <div className="container mx-auto grid grid-cols-1 gap-8 px-4 md:grid-cols-3">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="overflow-hidden rounded-lg bg-white shadow-md transition-transform hover:z-50 hover:scale-110"
+          >
+            <div className="relative h-64 w-full">
+              <Image
+                src={project.image}
+                alt={project.title}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-lg"
+              />
             </div>
-        </div>
-    );
+            <div className="p-6">
+              <h2 className="mb-2 text-2xl font-bold">{project.title}</h2>
+              <p className="text-gray-700">{project.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
