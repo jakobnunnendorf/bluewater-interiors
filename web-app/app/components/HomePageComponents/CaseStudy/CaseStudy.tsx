6013c4f37a01d16440283d73a61f9f0069f6a5ab
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2024-08-22 19:42:46 +0600

    case stady section doen

--- web-app/app/components/HomePageComponents/CaseStudy/CaseStudy.tsx
+++ web-app/app/components/HomePageComponents/CaseStudy/CaseStudy.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import TextWindow from "@/app/components/HomePageComponents/CaseStudy/TextWindow";
 import CaseStudyVideo from "@/app/components/HomePageComponents/CaseStudy/CaseStudyVideo";
 import HeadingTextLg from "../../UI/HeadingTextLg";
 
 function CaseStudy() {
   return (
-    <section className="bg-gray-light-2 mt-24 py-28 text-white">
-      <header className="!text-left">
-        <HeadingTextLg title="A fictional case study" />
-        <h4 className="font-gilroy-light text-[30px] font-normal text-black">
-          Nautical Elegance
-        </h4>
-      </header>
-      <div className="md:grid md:grid-cols-2">
+    <section className="bg-gray-light-2 mb-52 mt-24 py-12 lg:h-[665px] lg:pt-9">
+      <div className="flex w-full flex-col gap-11 max-xl:container xl:mx-auto xl:max-w-[1233px]">
+        <header className="!text-left">
+          <HeadingTextLg title="A fictional case study" />
+          <h4 className="font-gilroy-light text-[30px] font-normal text-black">
+            Nautical Elegance
+          </h4>
+        </header>
         <CaseStudyVideo />
-        <TextWindow />
       </div>
     </section>
   );
