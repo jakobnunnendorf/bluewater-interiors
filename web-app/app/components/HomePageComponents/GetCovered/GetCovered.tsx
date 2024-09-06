0af817839ae4cfa04b888655fbadaf24f0d5cbbe
Jakob <jnunnendorf@icloud.com>
2024-09-06 20:40:23 +0800

    fix(GetCovered.tsx): escape single quote character

--- web-app/app/components/HomePageComponents/GetCovered/GetCovered.tsx
+++ web-app/app/components/HomePageComponents/GetCovered/GetCovered.tsx
@@ -9,11 +9,11 @@ function GetCovered() {
         <div className="w-full max-w-[684px]">
           <header className="flex flex-col gap-2.5">
             <h2 className="font-gilroy-regular text-3xl font-medium uppercase leading-none text-black min-[477px]:text-5xl sm:text-[56px]">
-              <span className="text-primary-blue font-semibold">Let's get</span>{" "}
+              <span className="text-primary-blue font-semibold">Let&apos;s get</span>{" "}
               you covered
             </h2>
             <p className="font-gilroy-regular text-lg font-semibold text-primary-orange sm:text-lg md:text-xl lg:text-2xl">
-              Let's send your traffic cruising.
+              Let&apos;s send your traffic cruising.
             </p>
           </header>
           <div className="mt-5 flex flex-col gap-3.5">
